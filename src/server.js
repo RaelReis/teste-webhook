4f77c32f31efec4ab2051062c7e6a9b69f33c416
RaelReis <rafaelxdr1xdr1@gmail.com>
2023-08-15 19:07:37 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 4003;
+
+app.use(cors(), express.json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World",
+  });
+});
+
+app.post("/webhook", (req, res) => {
+  // Lógica para processar os dados do webhook
+  console.log("Recebido um webhook:", req.body);
+
+  // Responda ao webhook
+  res.status(200).send("Webhook recebido com sucesso!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening in http://localhost:${PORT}`);
+});
